ec9b039f3d90bc1a23acc4c4686846397ff42b82
Devank Singh <devankrf@gmail.com>
2026-01-31 18:26:17 +0530

    H2: Refactoring

--- asm8086_decoder.cpp
+++ asm8086_decoder.cpp
@@ -5,6 +5,84 @@
 #include <cstdint>
 #include <vector>
 
+constexpr uint8_t opcode_mov_reg_mem = 136;
+constexpr uint8_t opcode_mov_immediate_to_reg_mem = 198;
+constexpr uint8_t opcode_mov_immediate_to_reg = 176;
+
+constexpr uint8_t mod_mem_no = 0;
+constexpr uint8_t mod_mem_8_bit = 1;
+constexpr uint8_t mod_mem_16_bit = 2;
+constexpr uint8_t mod_register = 3;
+
+constexpr const char* reg_string[8][2] = {
+  { "al", "ax" },
+  { "cl", "cx" },
+  { "dl", "dx" },
+  { "bl", "bx" },
+  { "ah", "sp" },
+  { "ch", "bp" },
+  { "dh", "si" },
+  { "bh", "di" },
+};
+
+void processMovRegMem(const std::vector<uint8_t>& buffer, size_t& index) {
+  uint8_t low_byte = buffer[index++];
+  uint8_t high_byte = buffer[index++];
+
+  uint8_t mod = high_byte >> 6;
+  uint8_t reg = (high_byte >> 3) & 0x7;
+  uint8_t r_m = high_byte & 0x7;
+  uint8_t flag_w = low_byte & 1;
+  uint8_t flag_d = low_byte & 2;
+
+  std::string source;
+  std::string destination;
+  if (flag_d) {
+    source      = reg_string[r_m][flag_w];
+    destination = reg_string[reg][flag_w];
+  } else {
+    source      = reg_string[reg][flag_w];
+    destination = reg_string[r_m][flag_w];
+  }
+
+  std::cout << "mov ";
+  std::cout <<  destination << ", ";
+  std::cout <<  source << " ";
+  std::cout << std::endl;
+}
+
+void processMovImmediateReg(const std::vector<uint8_t>& buffer, size_t& index) {
+
+}
+
+int processMoveOperation(const std::vector<uint8_t>& buffer) {
+  size_t size = buffer.size();
+  size_t index = 0;
+  while (index < size) {
+    uint8_t low_byte = buffer[index];
+    if (low_byte & opcode_mov_reg_mem) 
+    {
+      processMovRegMem(buffer, index);
+    } 
+    else if (low_byte & opcode_mov_immediate_to_reg_mem) 
+    {
+      std::cerr << "Not implemented" << std::endl;
+      return 1;
+    } 
+    else if (low_byte & opcode_mov_immediate_to_reg) 
+    {
+      std::cerr << "Not implemented" << std::endl;
+      return 1;
+    } 
+    else 
+    {
+      std::cerr << "Operation is not move operation" << std::endl;
+      return 1;
+    }
+  }
+  return 0;
+}
+
 int main(int argc, char* argv[]) {
   if (argc < 2) {
     std::cerr << "Usage: " << argv[0] << " <assembled file>" << std::endl;
@@ -16,80 +94,19 @@ int main(int argc, char* argv[]) {
     std::cerr << "Could not open file: " << argv[1] << std::endl;
     return 1;
   }
-  
+
   file.seekg(0, std::ios::end);
   int fileSize = file.tellg();
   file.seekg(0, std::ios::beg);
 
-  if (fileSize % sizeof(uint16_t) != 0) {
-    std::cerr << "File size is not aligned to 16-bit integers\n";
-    return 1;
-  }
+  std::vector<uint8_t> buffer(fileSize);
 
-  std::size_t count = fileSize / sizeof(uint16_t);
-  std::vector<uint16_t> data(count);
-
-  file.read(reinterpret_cast<char*>(data.data()), fileSize);
-
-  constexpr uint8_t opcode_mov = 136;
-  constexpr uint8_t d = 2;
-  constexpr uint8_t w = 1;
-  constexpr uint8_t mod_mem_no = 0;
-  constexpr uint8_t mod_mem_8_bit = 1;
-  constexpr uint8_t mod_mem_16_bit = 2;
-  constexpr uint8_t mod_register = 3;
-  constexpr const char* reg_string[8][2] = {
-    { "al", "ax" },
-    { "cl", "cx" },
-    { "dl", "dx" },
-    { "bl", "bx" },
-    { "ah", "sp" },
-    { "ch", "bp" },
-    { "dh", "si" },
-    { "bh", "di" },
-  };
+  file.read(reinterpret_cast<char*>(buffer.data()), buffer.size());
 
   //std::cout << "; " << argv[1] << std::endl << std::endl;
   std::cout << "bits 16" << std::endl << std::endl;
 
-  for (uint16_t word : data) {
-    uint8_t high_byte = static_cast<uint8_t>(word >> 8);
-    uint8_t low_byte = static_cast<uint8_t>(word & 0xFF);
-    /*
-    std::cout << "word: " << std::to_string(word) << std::endl;
-    std::cout << "high_byte: " << std::to_string(high_byte) << std::endl;
-    std::cout << "low_byte: " << std::to_string(low_byte) << std::endl;
-    */
-    
-    if ((low_byte & opcode_mov) == 0) {
-      std::cerr << "Operation is not move operation" << std::endl;
-      return 1;
-    }
+  int result = processMoveOperation(buffer);
 
-    uint8_t mod = high_byte >> 6;
-    uint8_t reg = (high_byte >> 3) & 0x7;
-    uint8_t r_m = high_byte & 0x7;
-
-    if ((mod & mod_register) == 0) {
-      std::cerr << "Operation is not register move" << std::endl;
-      return 1;
-    }
-
-    std::string source;
-    std::string destination;
-    if (low_byte & d) {
-      source      = reg_string[r_m][low_byte & w];
-      destination = reg_string[reg][low_byte & w];
-    } else {
-      source      = reg_string[reg][low_byte & w];
-      destination = reg_string[r_m][low_byte & w];
-    }
-
-    std::cout << "mov ";
-    std::cout <<  destination << ", ";
-    std::cout <<  source << " ";
-    std::cout << std::endl;
-  }
-
-  return 0;
+  return result;
 }
