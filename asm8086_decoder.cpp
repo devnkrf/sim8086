01ed2c1005489d231ba6c78369a17b937fcf6c07
Devank Singh <devankrf@gmail.com>
2026-02-02 22:05:59 +0530

    H3: [WIP] Partial/Incorrect Solution

--- asm8086_decoder.cpp
+++ asm8086_decoder.cpp
@@ -4,11 +4,16 @@
 #include <string>
 #include <cstdint>
 #include <vector>
-//#include <bitset>
+#include <bitset>
 
 constexpr uint8_t opcode_mov_reg_mem = 136;
 constexpr uint8_t opcode_mov_immediate_to_reg_mem = 198;
 constexpr uint8_t opcode_mov_immediate_to_reg = 176;
+constexpr uint8_t opcode_arith_immediate_to_reg_mem = 128;
+
+constexpr uint8_t op_arith_add = 0;
+constexpr uint8_t op_arith_sub = 5;
+constexpr uint8_t op_arith_cmp = 7;
 
 constexpr uint8_t mod_mem_no = 0;
 constexpr uint8_t mod_mem_8_bit = 1;
@@ -37,10 +42,14 @@ constexpr const char* effective_addr_string[8] = {
   "bx"
 };
 
-void printMovString(const std::string& destination, const std::string& source) {
-  std::cout << "mov ";
+void printOpString(const std::string& operation, const std::string& destination, const std::string& source) {
+  std::cout << operation << " ";
   std::cout <<  destination << ", ";
   std::cout <<  source << std::endl;
+}
+
+constexpr bool hasOpcode(uint8_t value, uint8_t opcode) {
+  return (value & opcode) == opcode;
 }
 
 void processMovRegMem(const std::vector<uint8_t>& buffer, size_t& index) {
@@ -93,9 +102,9 @@ void processMovRegMem(const std::vector<uint8_t>& buffer, size_t& index) {
   }
 
   if (flag_d) {
-    printMovString(destination, source);
+    printOpString("mov", destination, source);
   } else {
-    printMovString(source, destination);
+    printOpString("mov", source, destination);
   }
 }
 
@@ -118,14 +127,84 @@ void processMovImmediateReg(const std::vector<uint8_t>& buffer, size_t& index) {
     source = std::to_string(data_1);
   }
 
-  printMovString(destination, source);
+  printOpString("mov", destination, source);
 }
 
-constexpr bool hasOpcode(uint8_t value, uint8_t opcode) {
-  return (value & opcode) == opcode;
+void opImmediateToMem(const std::vector<uint8_t>& buffer, size_t& index) {
+  uint8_t low_byte  = buffer[index++];
+  uint8_t high_byte = buffer[index++];
+
+  bool flag_w = low_byte & 1;
+  uint8_t mod = high_byte >> 6;
+  uint8_t r_m = high_byte & 7;
+  uint8_t op  = (high_byte >> 3) & 7;
+
+  uint8_t disp_hi = 0;
+  uint8_t disp    = 0;
+
+  if (mod == mod_mem_8_bit || mod == mod_mem_16_bit) {
+    disp = buffer[index++];
+  }
+  if (mod == mod_mem_16_bit) {
+    disp_hi = buffer[index++];
+    disp = static_cast<uint16_t>(disp_hi << 8) | disp;
+  }
+
+  std::string prefix = "byte";
+
+  uint8_t data_1 = 0;
+  uint8_t data   = 0;
+
+  data = buffer[index++];
+  if (flag_w) {
+    prefix = "word";
+    data_1 = buffer[index++];
+    data = static_cast<uint16_t>(data_1 << 8) | data;
+  }
+
+  std::string source = prefix + " " + std::to_string(data);
+  std::string destination;
+
+  if (mod == mod_register)
+  {
+    destination = reg_string[r_m][flag_w];
+  }
+  else if (mod == mod_mem_16_bit || mod == mod_mem_8_bit)
+  {
+    std::string suffix = disp ? std::string(" + ") + std::to_string(disp) : "";
+    destination = std::string("[") + effective_addr_string[r_m] + suffix + "]";
+  }
+  else if (mod == mod_mem_no && r_m == 6)
+  {
+    destination = std::string("[") + std::to_string(disp) + "]";
+  }
+  else
+  {
+    destination = std::string("[") + effective_addr_string[r_m] + "]";
+  }
+
+  std::string opString;
+  if (hasOpcode(low_byte, opcode_mov_immediate_to_reg_mem))
+  {
+    opString = "mov";
+  }
+  else if (op == op_arith_add)
+  {
+    opString = "add";
+  }
+  else if (op == op_arith_sub)
+  {
+    opString = "sub";
+  }
+  else if (op == op_arith_cmp)
+  {
+    opString = "cmp";
+  }
+
+  printOpString(opString, destination, source);
 }
 
-int processMoveOperation(const std::vector<uint8_t>& buffer) {
+int processOperations(const std::vector<uint8_t>& buffer) {
   size_t size = buffer.size();
   size_t index = 0;
   while (index < size) {
@@ -138,14 +217,17 @@ int processMoveOperation(const std::vector<uint8_t>& buffer) {
     {
       processMovRegMem(buffer, index);
     }
-    else if (low_byte & opcode_mov_immediate_to_reg_mem)
+    else if (hasOpcode(low_byte, opcode_mov_immediate_to_reg_mem))
     {
-      std::cerr << "Not implemented" << std::endl;
-      return 1;
+      opImmediateToMem(buffer, index);
+    }
+    else if (hasOpcode(low_byte, opcode_arith_immediate_to_reg_mem))
+    {
+      opImmediateToMem(buffer, index);
     }
     else
     {
-      std::cerr << "Operation [" << std::to_string(low_byte) << "] is not move operation" << std::endl;
+      std::cerr << "Operation [" << std::to_string(low_byte) << "] is unknown operation" << std::endl;
       return 1;
     }
   }
@@ -172,17 +254,15 @@ int main(int argc, char* argv[]) {
 
   file.read(reinterpret_cast<char*>(buffer.data()), buffer.size());
 
-  /*
   for (uint8_t b : buffer) {
     std::cout << std::bitset<8>(b) << " ";
   }
   std::cout << std::endl << std::endl;
-  */
 
   //std::cout << "; " << argv[1] << std::endl << std::endl;
   std::cout << "bits 16" << std::endl << std::endl;
 
-  int result = processMoveOperation(buffer);
+  int result = processOperations(buffer);
 
   return result;
 }
