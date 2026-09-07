e8a572c534521dd423e1c1e694305c74f2d58384
Devank Singh <devankrf@gmail.com>
2026-09-07 22:15:27 +0530

    P2-H2: Lexer basics

---
+++ part2/String.cpp
@@ -0,0 +1,28 @@
+#include <cstdlib>
+#include <cstring>
+
+struct String {
+  char *value;
+  long size;
+};
+
+// remember to clear it
+String makeString(const char *str, long size) {
+  String myStr;
+  myStr.value = (char *)malloc(size);
+  memcpy(myStr.value, str, size);
+  myStr.size = size;
+  return myStr;
+}
+
+bool isStringEq(String A, String B) {
+  if (A.size != B.size) {
+    return false;
+  }
+  for (int i = 0; i < A.size; i++) {
+    if (A.value[i] != B.value[i]) {
+      return false;
+    }
+  }
+  return true;
+}
---
+++ part2/json_parser.cpp
@@ -0,0 +1,134 @@
+#include <cctype>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+
+#include "String.cpp"
+
+constexpr auto filePath = "test.json";
+const String trueStr = makeString("true", 4);
+const String falseStr = makeString("false", 5);
+const String nullStr = makeString("null", 4);
+
+int readJsonFile(String *fileBuffer) {
+  FILE *fptr = fopen(filePath, "rb");
+  if (!fptr) {
+    printf("Unable to open file: %s\n", filePath);
+    return 1;
+  }
+
+  fseek(fptr, 0, SEEK_END);
+  long size = ftell(fptr);
+  rewind(fptr);
+  if (size <= 0) {
+    printf("Empty File: %s\n", filePath);
+    return 1;
+  }
+
+  fileBuffer->value = (char *)malloc(size);
+  if (!fileBuffer->value) {
+    printf("Unable to allocate fileBuffer of size %ld\n", size);
+    return 1;
+  }
+
+  size_t bytes_read = fread(fileBuffer->value, 1, size, fptr);
+  if (bytes_read != (size_t)size) {
+    printf("Bytes read [%ld] not equal to file size [%ld]\n", bytes_read, size);
+    return 1;
+  }
+  fileBuffer->size = size;
+
+  fclose(fptr);
+  return 0;
+}
+
+bool lexer_tokenize_bool(String *fileBuffer, String ref, int i) {
+  if (i + ref.size > fileBuffer->size) {
+    return false;
+  }
+  if (!isStringEq({fileBuffer->value + i, trueStr.size}, ref)) {
+    return false;
+  }
+  return true;
+}
+
+void lexer_tokenize(String *fileBuffer) {
+  printf("Original Json\n%.*s\n", (int)fileBuffer->size, fileBuffer->value);
+
+  printf("Tokens:\n");
+  for (int i = 0; i < fileBuffer->size; i++) {
+    // Braces
+    if (fileBuffer->value[i] == '{') {
+      printf("START_CURLY_BRACE ");
+    } else if (fileBuffer->value[i] == '}') {
+      printf("END_CURLY_BRACE ");
+    } else if (fileBuffer->value[i] == '[') {
+      printf("START_SQUARE_BRACE ");
+    } else if (fileBuffer->value[i] == ']') {
+      printf("END_SQUARE_BRACE ");
+    }
+    // Seperators
+    else if (fileBuffer->value[i] == ',') {
+      printf("COMMA ");
+    } else if (fileBuffer->value[i] == ':') {
+      printf("COLON ");
+    }
+    // Number, String, Boolean, Null
+    else if (isdigit(fileBuffer->value[i]) || fileBuffer->value[i] == '-') {
+      int start = i;
+      while (i + 1 < fileBuffer->size && (isdigit(fileBuffer->value[i + 1]) ||
+                                          fileBuffer->value[i + 1] == '.')) {
+        i++;
+      }
+      printf("NUMBER(%.*s) ", i - start + 1, fileBuffer->value + start);
+    } else if (fileBuffer->value[i] == '"') {
+      int start = i;
+      do {
+        i++;
+      } while (i < fileBuffer->size && fileBuffer->value[i] != '"');
+      printf("STRING(%.*s) ", i - start + 1, fileBuffer->value + start);
+    } else if (fileBuffer->value[i] == 't') {
+      if (!lexer_tokenize_bool(fileBuffer, trueStr, i)) {
+        printf("\nLexer Error: Unkown t at _,_ \n");
+        return;
+      }
+      i += 3;
+      printf("VAL_TRUE ");
+    } else if (fileBuffer->value[i] == 'f') {
+      // false
+      if (!lexer_tokenize_bool(fileBuffer, trueStr, i)) {
+        printf("\nLexer Error: Unkown f at _,_ \n");
+        return;
+      }
+      i += 4;
+      printf("VAL_FALSE");
+    } else if (fileBuffer->value[i] == 'n') {
+      // null
+      if (!lexer_tokenize_bool(fileBuffer, nullStr, i)) {
+        printf("\nLexer Error: Unkown n at _,_ \n");
+        return;
+      }
+      i += 3;
+      printf("VAL_NULL");
+    }
+
+    // Skip Whitespace
+    else if (fileBuffer->value[i] == ' ') {
+    } else if (fileBuffer->value[i] == '\n') {
+    } else if (fileBuffer->value[i] == '\r') {
+    } else if (fileBuffer->value[i] == '\t') {
+    }
+  }
+}
+
+int main() {
+  String fileBuffer = {};
+  int fr_res = readJsonFile(&fileBuffer);
+  if (fr_res == 0) {
+    lexer_tokenize(&fileBuffer);
+  }
+  if (fileBuffer.value) {
+    free(fileBuffer.value);
+  }
+  return fr_res;
+}
